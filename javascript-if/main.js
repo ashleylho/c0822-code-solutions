7894795e36d803650aa4dd8c1bbb530000bd1e7e
Ashley Ho <ashley.lho@gmail.com>
2022-08-31 17:43:09 +0000

    fixed the categorizeAcidity function conditions

--- javascript-if/main.js
+++ javascript-if/main.js
@@ -62,9 +62,9 @@ function isOldEnoughToDrinkAndDrive(person) {
 function categorizeAcidity(pH) {
   if (pH === 7) {
     return 'neutral';
-  } else if (pH > 0 && pH < 7) {
+  } else if (pH >= 0 && pH < 7) {
     return 'acid';
-  } else if (pH > 7 && pH < 14) {
+  } else if (pH > 7 && pH <= 14) {
     return 'base';
   } else {
     return 'invalid pH level';
