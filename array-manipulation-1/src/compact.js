0bfc2d2f61e7b184e5e1c7a445489f21634c8a7a
Ashley Ho <ashley.lho@gmail.com>
2022-09-06 04:23:07 +0000

    completed array-manipulation-1 exercise

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -4,3 +4,13 @@
 // if it is a value of false, NaN, null, 0, -0, undefined, ' ' remove from the array
 // if it is neither of those, push into the output
 // return output
+
+function compact(array) {
+  var compactResult = [];
+  for (var i = 0; i < array.length; i++) {
+    if (array[i]) {
+      compactResult.push(array[i]);
+    }
+  }
+  return compactResult;
+}
