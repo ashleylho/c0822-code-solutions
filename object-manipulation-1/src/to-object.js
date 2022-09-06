d6629a6f4577e0959b7e49c9459331f988b81dd4
Ashley Ho <ashley.lho@gmail.com>
2022-09-06 04:55:51 +0000

    completed object-manipulation-1 exercise

--- object-manipulation-1/src/to-object.js
+++ object-manipulation-1/src/to-object.js
@@ -2,10 +2,11 @@
 // create storage for output (object)
 // look into array
 // grab both the key and value pair
-// push them both into the ouput
+// add them both into the ouput
 // show output
 
 function toObject(keyValuePair) {
   var toObjectResult = {};
+  toObjectResult[keyValuePair[0]] = keyValuePair[1];
   return toObjectResult;
 }
