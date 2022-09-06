74096e3bbfdc61ef1a338fed7f0056cf12998030
Ashley Ho <ashley.lho@gmail.com>
2022-09-06 21:56:10 +0000

    completed dom-manipulation exercise

---
+++ dom-manipulation/main.js
@@ -0,0 +1,21 @@
+var clickCount = 0;
+
+var $hotButton = document.querySelector('.hot-button');
+var $clickCount = document.querySelector('.click-count');
+
+function incrementCounter(event) {
+  $clickCount.textContent = 'Clicks: ' + clickCount++;
+  if (clickCount < 7) {
+    $hotButton.className = 'hot-button cool';
+  } else if (clickCount < 10) {
+    $hotButton.className = 'hot-button tepid';
+  } else if (clickCount < 13) {
+    $hotButton.className = 'hot-button warm';
+  } else if (clickCount < 16) {
+    $hotButton.className = 'hot-button hot';
+  } else {
+    $hotButton.className = 'hot-button nuclear';
+  }
+}
+
+$hotButton.addEventListener('click', incrementCounter);
