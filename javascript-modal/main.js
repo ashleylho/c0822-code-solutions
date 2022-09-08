8d95a760d46557139b58b5eb848e09ab51c6e006
Ashley Ho <ashley.lho@gmail.com>
2022-09-08 02:17:09 +0000

    completed javascript-modal exercise

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -1,8 +1,13 @@
 var $background = document.querySelector('.background');
 var $open = document.querySelector('.open');
-// var $no = document.querySelector('.no');
+var $no = document.querySelector('.no');
 
 function handleClick(event) {
   $background.className = 'backgroundclick';
 }
 $open.addEventListener('click', handleClick);
+
+function handleClickOff(event) {
+  $background.className = 'background';
+}
+$no.addEventListener('click', handleClickOff);
