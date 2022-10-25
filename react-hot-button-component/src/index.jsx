c76f062df09ed64cdd6f945942e15817a10bc3dc
Ashley Ho <ashley.lho@gmail.com>
2022-10-25 18:23:05 +0000

    completed react-hot-button-component challenge

---
+++ react-hot-button-component/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom/client';
+import HotButton from './hot-button';
+
+const container = document.querySelector('#root');
+const root = ReactDom.createRoot(container);
+
+root.render(<HotButton />);
