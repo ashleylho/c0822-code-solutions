7d36584f8b405b68945d12369bc4fd5fcc284408
Ashley Ho <ashley.lho@gmail.com>
2022-10-31 21:03:42 +0000

    updated render and openTopic methods so details open on click

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -8,21 +8,24 @@ export default class Accordion extends React.Component {
   }
 
   openTopic(event) {
-    this.setState({ clickedTopicId: event.target.id });
-  }
-
-  closeTopic(event) {
-
+    if (this.state.clickedTopicId === null) {
+      this.setState({ clickedTopicId: event.target.dataset.topicId });
+    } else if (event.target.dataset.topicId === this.state.clickedTopicId) {
+      this.setState({ clickedTopicId: null });
+    }
   }
 
   render() {
     const topics = this.props.topics;
-    const listItems = topics.map(topic =>
-      <li key={topic.id} data-topic-id={topic.id}>
-        <div className='topic' onClick={this.openTopic}>
+    const listItems = topics.map(topic => {
+      const toggle = Number(this.state.clickedTopicId) === topic.id ? '' : 'hidden';
+      return <li key={topic.id}>
+        <div data-topic-id={topic.id} className='topic' onClick={this.openTopic}>
         {topic.name}
         </div>
-        </li>);
+        <p className={toggle}>{topic.details}</p>
+        </li>;
+    });
     return (
       <ul>
         {listItems}
