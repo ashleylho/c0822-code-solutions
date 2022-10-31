0cfe5e50dc77a17665d812d93ff06e99f3c26f94
Ashley Ho <ashley.lho@gmail.com>
2022-10-31 17:34:28 +0000

    updated state in constructor function

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 export default class Accordion extends React.Component {
   constructor(props) {
     super(props);
-    this.status = { isOpen: false };
+    this.state = { clickedTopicId: null };
     this.openTopic = this.openTopic.bind(this);
   }
 
   openTopic(event) {
-    this.setState({ isOpen: true });
+    this.setState({ clickedTopicId: event.target.id });
   }
 
   closeTopic(event) {
@@ -18,11 +18,10 @@ export default class Accordion extends React.Component {
   render() {
     const topics = this.props.topics;
     const listItems = topics.map(topic =>
-      <li key={topic.id}>
+      <li key={topic.id} data-topic-id={topic.id}>
         <div className='topic' onClick={this.openTopic}>
         {topic.name}
         </div>
-        <p className={`details ${this.status.isOpen ? '' : 'hidden'}`}>{topic.details}</p>
         </li>);
     return (
       <ul>
@@ -31,6 +30,3 @@ export default class Accordion extends React.Component {
     );
   }
 }
-
-// map through topics, if event.target === topic, generate topic details for that topic
-// else just generate topics
