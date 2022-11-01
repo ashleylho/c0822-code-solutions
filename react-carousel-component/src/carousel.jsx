f7f78f82aec5f71f8ced2862a97e75c0da45b4f7
Ashley Ho <ashley.lho@gmail.com>
2022-11-01 22:04:17 +0000

    react-carousel-component progress: added images, icons, and method for arrow clicking

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -1,9 +1,63 @@
 import React from 'react';
 
 export default class Carousel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { currentImg: 1 };
+    this.carousel = this.carousel.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.handleArrow = this.handleArrow.bind(this);
+  }
+
+  carousel() {
+
+  }
+
+  handleClick(event) {
+
+  }
+
+  handleArrow(event) {
+    if (event.target.className.includes('fa-chevron-right')) {
+      if (this.state.currentImg !== 5) {
+        this.setState({ currentImg: this.state.currentImg + 1 });
+      } else {
+        this.setState({ currentImg: 1 });
+      }
+    } else if (event.target.className.includes('fa-chevron-left')) {
+      if (this.state.currentImg !== 1) {
+        this.setState({ currentImg: this.state.currentImg - 1 });
+      } else {
+        this.setState({ currentImg: 5 });
+      }
+    }
+  }
+
   render() {
+    const imgSrc = this.props.imgSrc;
+    const imageList = imgSrc.map(img => {
+      const toggle = this.state.currentImg === img.id ? '' : 'hidden';
+      return <img key={img.id} className={toggle} src={img.img}></img>;
+    });
     return (
-      <div>hi</div>
+      <>
+        <div className="row" onClick={this.handleArrow}>
+        <div className='column-full arrows' >
+          <i className='fa-solid fa-chevron-left'></i>
+          {imageList}
+          <i className='fa-solid fa-chevron-right'></i>
+        </div>
+      </div>
+      <div className="row" onClick={this.handleClick}>
+        <div className="column-full circle">
+          <i className="fa-solid fa-circle"></i>
+          <i className="fa-regular fa-circle"></i>
+          <i className="fa-regular fa-circle"></i>
+          <i className="fa-regular fa-circle"></i>
+          <i className="fa-regular fa-circle"></i>
+        </div>
+      </div>
+      </>
     );
   }
 }
--- react-carousel-component/src/index.jsx
+++ react-carousel-component/src/index.jsx
@@ -2,6 +2,29 @@ import React from 'react';
 import ReactDom from 'react-dom/client';
 import Carousel from './carousel';
 
+const images = [
+  {
+    id: 1,
+    img: 'https://preview.redd.it/g2nltjddg5e41.jpg?auto=webp&s=5f168221264a4e4fd00c4026b880a9a1beb94cae'
+  },
+  {
+    id: 2,
+    img: 'https://i.pinimg.com/originals/2d/60/df/2d60df43e06b1735adbe5c234f69cde3.jpg'
+  },
+  {
+    id: 3,
+    img: 'http://mymodernmet.com/wp/wp-content/uploads/2017/03/camerons-chinchillas-butt-2.jpg'
+  },
+  {
+    id: 4,
+    img: 'https://pbs.twimg.com/media/EZHN_WGXYAUTtt9?format=jpg&name=large'
+  },
+  {
+    id: 5,
+    img: 'https://pbs.twimg.com/media/EAEQ_ciX4AAIITj.jpg'
+  }
+];
+
 const container = document.querySelector('#root');
 const root = ReactDom.createRoot(container);
-root.render(<Carousel />);
+root.render(<Carousel imgSrc={images}/>);
